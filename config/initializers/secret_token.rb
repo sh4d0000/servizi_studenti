e154d865ada7e9cfa57f7ccf6386ae9bbef8f7f5
Antonelli <me@antonioantonelli.it>
2012-03-11 14:57:50 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServiziStudenti::Application.config.secret_token = 'e6812aa1420015de941876603aad8b07b3c652b7537fae4681dd45705544f8e3424fda50e11c75a70665f9524a25738d089b7bd550bebd66b8d436cdd38149b1'
